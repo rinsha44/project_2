19f1b00cfab6172e53d551fc0a23af3fb99ae9d1
rinsha k <rinshakabeer14@gmail.com>
2022-10-30 21:49:15 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+    home: Splash(),
+  ));
+}
+class Splash  extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:const [
+            Icon(Icons.ac_unit, size: 30, color: Colors.indigo,),
+            Text("SAMPLE APP",
+              style: TextStyle(color: Colors.orangeAccent, fontSize: 30),),
+          ],
+        ),
+      ),
+    );
+  }
+}
